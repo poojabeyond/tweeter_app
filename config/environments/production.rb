ef57f67752bad1dfb96412ecb16f05764bf34d25
poojabeyond <pooja@beyondfinity.com>
2024-01-05 15:40:15 +0530

    secret key add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.secret_key_base = Rails.application.credentials.production[:secret_key_base]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

275fa5596de7c4f386dfed543482b2901dc61cf1
poojabeyond <pooja@beyondfinity.com>
2024-01-09 10:39:48 +0530

    spel check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ config.action_mailer.default_url_options = { host: host }
 config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  user_name: 'pooja@beyondfinit.com',
+  user_name: 'pooja@beyondfinity.com',
   password: 'hmxs qjzz uipy qubo',
   authentication: 'plain',
   enable_starttls_auto: true

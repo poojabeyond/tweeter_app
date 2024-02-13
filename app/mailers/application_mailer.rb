33d5500bb0043be37bd01add71b2372140502ec6
poojabeyond <pooja@beyondfinity.com>
2024-02-13 19:15:54 +0530

    Production mailer setup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "pooja@beyondfinity.com"
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://rails-tweeter-35a3d4a52e03.herokuapp.com/'
+   config.action_mailer.default_url_options = { host: host }
+   ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => 'apikey',
+  :password => ENV['SENDGRID_API_KEY'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
